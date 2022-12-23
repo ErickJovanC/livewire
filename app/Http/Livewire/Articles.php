0e26b87b5584368c18d86c9b312cd08f96abc653
Erick Jovan <erickjovan106@gmail.com>
2022-12-23 08:28:50 -0600

    Firts componete livewire

---
+++ app/Http/Livewire/Articles.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Articles extends Component
+{
+    public function render()
+    {
+        return view('livewire.articles', [
+            'articles' => ['uno', 'dos', 'tres']
+        ]);
+    }
+}
---
+++ resources/views/livewire/articles.blade.php
@@ -0,0 +1,5 @@
+<div>
+    @foreach ($articles as $article)
+        {{ $article; }}<br>
+    @endforeach
+</div>
