cf4e3f3d6cdeae9aeb4fbf70b534e57b1928a2bc
Erick Jovan <erickjovan106@gmail.com>
2022-12-29 00:33:05 -0600

    view personalized with bootstrap

--- resources/views/livewire/articles.blade.php
+++ resources/views/livewire/articles.blade.php
@@ -1,79 +1,85 @@
-<div>
-    <div class="flex items-center">
-        <input type="text" wire:model="search" placeholder="Busqueda...">
+<div class="row">
+    <div class="col-md-10 mb-3">
+        <input type="text" wire:model="search" placeholder="Busqueda..." class="form-control bg-dark text-white">
+    </div>
+    <div class="col-md-2 text-end mb-3">
         @livewire('create-post')
     </div>
-    @if ($posts->count())
-        <table class="min-w-full">
-            <thead class="bg-white border-b">
-                <tr>
-                    <th wire:click="order('id')" scope="col"
-                        class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                        {{-- Icons sort --}}
-                        @if ($sort == 'id')
-                            @if ($direction == 'asc')
-                                <i class="fas fa-sort-alpha-down float-right"></i>
-                            @else
-                                <i class="fas fa-sort-alpha-up float-right"></i>
-                            @endif
-                        @else
-                            <i class="fas fa-sort"></i>
-                        @endif
-                        #
-                    </th>
-                    <th wire:click="order('title')" scope="col"
-                        class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                        {{-- Icons sort --}}
-                        @if ($sort == 'title')
-                            @if ($direction == 'asc')
-                                <i class="fas fa-sort-alpha-down float-right"></i>
-                            @else
-                                <i class="fas fa-sort-alpha-up float-right"></i>
-                            @endif
-                        @else
-                            <i class="fas fa-sort"></i>
-                        @endif
-                        Title
-                    </th>
-                    <th wire:click="order('content')" scope="col"
-                        class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                        {{-- Icons sort --}}
-                        @if ($sort == 'content')
-                            @if ($direction == 'asc')
-                                <i class="fas fa-sort-alpha-down float-right"></i>
-                            @else
-                                <i class="fas fa-sort-alpha-up float-right"></i>
-                            @endif
-                        @else
-                            <i class="fas fa-sort"></i>
-                        @endif
-                        Content
-                    </th>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                        Edit
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($posts as $post)
-                    <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                            {{ $post->id }}
-                        </td>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            {{ $post->title }}
-                        </td>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            {{ $post->content }}
-                        </td>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            Edit
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @else
-        No se encontraron Registros
-    @endif
+    <div class="col-12">
+        @if ($posts->count())
+            <div class="table-responsive-xl">
+                <table class="table table-dark table-hover align-middle">
+                    <thead>
+                        <tr>
+                            <th wire:click="order('id')" scope="col"
+                                class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                {{-- Icons sort --}}
+                                @if ($sort == 'id')
+                                    @if ($direction == 'asc')
+                                        <i class="fas fa-sort-alpha-down float-right"></i>
+                                    @else
+                                        <i class="fas fa-sort-alpha-up float-right"></i>
+                                    @endif
+                                @else
+                                    <i class="fas fa-sort"></i>
+                                @endif
+                                #
+                            </th>
+                            <th wire:click="order('title')" scope="col"
+                                class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                {{-- Icons sort --}}
+                                @if ($sort == 'title')
+                                    @if ($direction == 'asc')
+                                        <i class="fas fa-sort-alpha-down float-right"></i>
+                                    @else
+                                        <i class="fas fa-sort-alpha-up float-right"></i>
+                                    @endif
+                                @else
+                                    <i class="fas fa-sort"></i>
+                                @endif
+                                Title
+                            </th>
+                            <th wire:click="order('content')" scope="col"
+                                class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                {{-- Icons sort --}}
+                                @if ($sort == 'content')
+                                    @if ($direction == 'asc')
+                                        <i class="fas fa-sort-alpha-down float-right"></i>
+                                    @else
+                                        <i class="fas fa-sort-alpha-up float-right"></i>
+                                    @endif
+                                @else
+                                    <i class="fas fa-sort"></i>
+                                @endif
+                                Content
+                            </th>
+                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                Edit
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                            <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                    {{ $post->id }}
+                                </td>
+                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    {{ $post->title }}
+                                </td>
+                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    {{ $post->content }}
+                                </td>
+                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    Edit
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @else
+            <div class="bg-dark text-white p-3">No se encontraron Registros</div>
+        @endif
+    </div>
 </div>
---
+++ resources/views/livewire/create-post.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addPost">Crear Nuevo Articulo</button>
+</div>
